99d48e1caabd66fb2bb4846e228769fae0f3c64e
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2022-10-14 10:22:47 +0900

    update

--- blockchain.go
+++ blockchain.go
@@ -3,13 +3,66 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init Hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp %d\n", b.timestamp)
+	fmt.Printf("nonce %d\n", b.nonce)
+	fmt.Printf("previousHash %s\n", b.previousHash)
+	fmt.Printf("transactions %s\n", b.transactions)
+}
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	log.Println("test")
-	fmt.Println("test2")
+	blockchain := NewBlockchain()
+	blockchain.Print()
+	blockchain.CreateBlock(5, "hash 1")
+	blockchain.Print()
+	blockchain.CreateBlock(2, "hash 2")
+	blockchain.Print()
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryotarohada/goblockchain
+
+go 1.18
